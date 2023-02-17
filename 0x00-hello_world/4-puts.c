89ded244cf6d18a6f84003a9602518a9728586ea
Yetunde <yetundemotun97@gmail.com>
2023-02-17 07:05:03 -0800

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -7,7 +6,6 @@
  */
 
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+{ puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
