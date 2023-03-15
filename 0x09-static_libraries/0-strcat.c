1ed949f9e4d01da45fecd0f5f15ff39a6ca30d07
Yetunde <yetundemotun97@gmail.com>
2023-03-15 14:11:27 -0700

    chnge

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,23 +1,30 @@
 #include "main.h"
+
 /**
- * _strcmp - compare string values
- * @s1: input value
- * @s2: input value
+ * _strcat- concatenates two strings
+ * @dest: input value
+ * @src: input value
  *
- * Return: s1[i] - s2[i]
+ * Return: void
  */
-int _strcmp(char *s1, char *s2)
+
+char *_strcat(char *dest, char *src)
 {
 	int i;
+	int j;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (dest[i] != '\0')
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-	i++;
+		i++;
 	}
-	return (0);
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
