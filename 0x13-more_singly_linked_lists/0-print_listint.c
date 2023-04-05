837523ded6e08e14cb1f95214a32496cc88c84a0
Yetunde Salaudeen <yetundemotun97@gmail.com>
2023-04-05 03:30:58 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint - function that prits all elements of listint_t
- * @h: the beginning of the list to print
- * Return: number of nodes
+ * print_listint - to print the elements of a linked list.
+ * @h: the linked list of listint_t to print.
+ * Return: the mumber of nodes (counting).
  */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t listing = 0;
+	size_t counting = 0;
 
 	while (h)
 	{
-		printf("%i\n", h->n);
+		printf("%d\n", h->n);
+		counting++;
 		h = h->next;
-		listing++;
 	}
-	return (listing);
+
+	return (counting);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef TABLE_N
+#define TABLE_N
+
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 /**
@@ -8,15 +11,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *
  */
-
 typedef struct listint_s
 {
-        int n;
-        struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -27,9 +29,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(listint_t **head, unsigned int index);
-listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t *head);
-listint_t *find_listint_loop(listint_t *head);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif
